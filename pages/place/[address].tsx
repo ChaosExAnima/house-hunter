509a1316cd38664c0fd631db92435fea6de4a491
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2022-05-27 20:10:23 -0400

    Starts fleshing out details page.

--- pages/place/[address].tsx
+++ pages/place/[address].tsx
@@ -1,5 +1,101 @@
-import Page from 'components/page';
+import { Skeleton, Typography } from '@mui/material';
+import { useQuery } from 'react-query';
 
-export default function Address() {
-	return <Page>Home title</Page>;
+import ErrorDisplay from 'components/error-display';
+import Link from 'components/link';
+import Page from 'components/page';
+import { fetchApi } from 'utils/fetch';
+
+import type { PlaceDetailsProps } from './types';
+import type {
+	GetStaticPathsResult,
+	GetStaticPropsContext,
+	GetStaticPropsResult,
+	InferGetStaticPropsType,
+} from 'next';
+import type { PlaceDetail } from 'pages/api/place/types';
+
+export default function PlaceDetails({
+	id,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+	const { status, data, error } = useQuery(`/place/${id}`, () =>
+		fetchApi<PlaceDetail>(`/place/${id}`),
+	);
+	if (status === 'loading') {
+		return <PlaceDetailsSkeleton />;
+	}
+	if (!data) {
+		return <PlaceDetailsLost />;
+	}
+	const { place } = data;
+	return (
+		<>
+			<ErrorDisplay error={error} />
+			<Page maxWidth="md">
+				<Typography variant="h3" component="h1">
+					{place.address}
+				</Typography>
+			</Page>
+		</>
+	);
+}
+
+export async function getStaticProps(
+	context: GetStaticPropsContext<{ address: string }>,
+): Promise<GetStaticPropsResult<PlaceDetailsProps>> {
+	if (!context.params?.address) {
+		return {
+			notFound: true,
+		};
+	}
+	const { address } = context.params;
+	if (!address) {
+		return {
+			notFound: true,
+		};
+	}
+	const { default: listings } = await import('data/listing-fixtures');
+	const listing = listings.find(({ slug }) => slug === address);
+	if (!listing) {
+		return {
+			notFound: true,
+		};
+	}
+	return {
+		props: {
+			id: listing.id,
+		},
+	};
+}
+
+export async function getStaticPaths(): Promise<GetStaticPathsResult> {
+	const { default: listings } = await import('data/listing-fixtures');
+	return {
+		paths: listings.map(({ slug }) => `/place/${slug}`),
+		fallback: true,
+	};
+}
+
+function PlaceDetailsSkeleton() {
+	return (
+		<Page maxWidth="md">
+			<Typography variant="h3" component="h1">
+				<Skeleton />
+			</Typography>
+		</Page>
+	);
+}
+
+function PlaceDetailsLost() {
+	return (
+		<Page>
+			<Typography variant="h3" component="h1" gutterBottom>
+				Are you lost?
+			</Typography>
+			<Typography>No listing here!</Typography>
+			<Typography>
+				<Link href="/">Go home</Link>
+			</Typography>
+		</Page>
+	);
 }
